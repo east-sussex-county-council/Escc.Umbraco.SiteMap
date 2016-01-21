4157b66fe87d8426a8be8a6d0b6947cfc8ef6fa1
Gordon Saxby <gordon.saxby@eastsussex.gov.uk>
2016-01-21 10:59:39 +0000

    Remove unnecessary AutoFac code

--- Escc.Umbraco.SiteMap/App_Start/EventHandler.cs
+++ Escc.Umbraco.SiteMap/App_Start/EventHandler.cs
@@ -1,16 +1,6 @@
-﻿using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using Autofac;
-using Autofac.Integration.Mvc;
-using Autofac.Integration.WebApi;
-using Escc.Umbraco.SiteMap.Controllers;
-using Escc.Umbraco.SiteMap.Services;
-using Escc.Umbraco.SiteMap.Services.Interfaces;
 using Umbraco.Core;
-using Umbraco.Core.Persistence;
-using Umbraco.Core.Services;
-using Umbraco.Web;
 
 namespace Escc.Umbraco.SiteMap
 {
@@ -34,25 +24,8 @@ namespace Escc.Umbraco.SiteMap
                     controller = "Sitemap",
                     action = "Index",
                     id = UrlParameter.Optional
-                });
-
-            var builder = new ContainerBuilder();
-
-            // web api controllers
-            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
-            builder.RegisterApiControllers(typeof(SitemapController).Assembly);
-
-            //add custom class to the container as Transient instance
-            builder.RegisterType<RepositoryFactory>();
-            builder.RegisterType<MediaService>().As<IMediaService>();
-            builder.RegisterType<MediaLibraryService>().As<IMediaLibraryService>();
-
-            var container = builder.Build();
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-
-            var resolver = new AutofacWebApiDependencyResolver(container);
-
-            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+                }
+            );
         }
     }
 }
